a2a9fbf69f1558a32a72de238bcd85a0aa0691ff
Fyrstikkeske <79742888+Fyrstikkeske@users.noreply.github.com>
2025-04-04 21:36:08 +0200

    polishing, i know you will make your own chud but the LegacyColorUtil might be something you like or can just optimise

--- src/main/java/com/flansmod/warforge/common/mixins/vanilla/RenderMixin.java
+++ src/main/java/com/flansmod/warforge/common/mixins/vanilla/RenderMixin.java
@@ -11,6 +11,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
+import static com.flansmod.warforge.client.util.LegacyColorUtil.getClosestLegacyColor;
+
 
 @Mixin(Render.class)
 public abstract class RenderMixin<T extends Entity>{
@@ -34,20 +36,18 @@ public abstract class RenderMixin<T extends Entity>{
 
         if (d0 <= (double)(maxDistance * maxDistance))
         {
-
-            String name = str;
-            Faction info = WarForgeMod.FACTIONS.getFactionOfPlayer(entityIn.getUniqueID());;
-            if (info != null){
-                name = str + " - " + info.name;
-            }
-
             boolean flag = entityIn.isSneaking();
             float f = this.renderManager.playerViewY;
             float f1 = this.renderManager.playerViewX;
             boolean flag1 = this.renderManager.options.thirdPersonView == 2;
             float f2 = entityIn.height + 0.5F - (flag ? 0.25F : 0.0F);
             int i = "deadmau5".equals(str) ? -10 : 0;
-            EntityRenderer.drawNameplate(this.getFontRendererFromRenderManager(), name, (float)x, (float)y + f2, (float)z, i, f, f1, flag1, flag);
+            EntityRenderer.drawNameplate(this.getFontRendererFromRenderManager(), str, (float)x, (float)y + f2, (float)z, i, f, f1, flag1, flag);
+            Faction info = WarForgeMod.FACTIONS.getFactionOfPlayer(entityIn.getUniqueID());
+            if (info != null){
+                String faction = getClosestLegacyColor(info.colour) + info.name;
+                EntityRenderer.drawNameplate(this.getFontRendererFromRenderManager(), faction, (float)x, (float)y + f2 - 0.23f, (float)z, i, f, f1, flag1, flag);
+            }
         }
     }
 }
