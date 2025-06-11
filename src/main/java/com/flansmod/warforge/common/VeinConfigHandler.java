78ccfaf2c52d1ba78784d336bb3b7cc6a1b27e7a
MrNorwood <[email protected]>
2025-06-11 15:37:55 +0200

    Java 8 compat

--- src/main/java/com/flansmod/warforge/common/VeinConfigHandler.java
+++ src/main/java/com/flansmod/warforge/common/VeinConfigHandler.java
@@ -3,16 +3,16 @@ package com.flansmod.warforge.common;
 import lombok.AllArgsConstructor;
 import org.yaml.snakeyaml.Yaml;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class VeinConfigHandler {
-    public final static String CONFIG_PATH = Paths.get("config/" + WarForgeMod.MODID + "/upgrade_levels.cfg").toString();
+    public final static String CONFIG_PATH = Paths.get("config/" + WarForgeMod.MODID + "/veins.cfg").toString();
 
     public static List<VeinEntry> loadVeins(String resourcePath) {
         List<VeinEntry> entries = new ArrayList<>();
@@ -23,32 +23,32 @@ public class VeinConfigHandler {
         }
 
         Yaml yaml = new Yaml();
-        List<Map<String,Object>> rawVeins;
-        try{
-        Map<String, Object> obj = yaml.load(inputStream);
-        rawVeins =  (List<Map<String, Object>>) obj.get("veins");
-        } catch (ClassCastException e){
-           WarForgeMod.LOGGER.error("Failed to parse veins: ",e);
-           return null;
+        List<Map<String, Object>> rawVeins;
+        try {
+            Map<String, Object> obj = yaml.load(inputStream);
+            rawVeins = (List<Map<String, Object>>) obj.get("veins");
+        } catch (ClassCastException e) {
+            WarForgeMod.LOGGER.error("Failed to parse veins: ", e);
+            return null;
         }
-        for(Map<String,Object> rawVeinData : rawVeins){
+        for (Map<String, Object> rawVeinData : rawVeins) {
             try {
                 String name = (String) rawVeinData.get("name");
                 List<Map<String, Object>> dimsRaw = (List<Map<String, Object>>) rawVeinData.get("dims");
                 List<DimWeight> dims = dimsRaw.stream().map(dim -> new DimWeight(
-                        ((Number) dim.get("id")).intValue(),
-                        ((Number) dim.get("weight")).doubleValue()))
-                        .toList();
+                                ((Number) dim.get("id")).intValue(),
+                                ((Number) dim.get("weight")).doubleValue()))
+                        .collect(Collectors.toList());
                 List<Map<String, Object>> componentsRaw = (List<Map<String, Object>>) rawVeinData.get("components");
                 List<Component> components = componentsRaw.stream()
                         .map(comp -> new Component(
                                 (String) comp.get("item"),
                                 ((Number) comp.get("yield")).intValue(),
                                 ((Number) comp.get("weight")).doubleValue()))
-                        .toList();
+                        .collect(Collectors.toList());
                 entries.add(new VeinEntry(name, dims, components));
-            } catch ( ClassCastException e){
-                WarForgeMod.LOGGER.error("Failed to parse vein: ",e);
+            } catch (ClassCastException e) {
+                WarForgeMod.LOGGER.error("Failed to parse vein: ", e);
                 continue;
             }
 
